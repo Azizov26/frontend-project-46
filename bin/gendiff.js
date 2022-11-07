1a00d57aad809c3ec8953b1ba8aff9546b789a59
Aziz <azizov-aziz58@yandex.ru>
2022-11-07 13:41:11 +0500

    step tree, prettirrc and eslint completed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import { Command } from "commander/esm.mjs";
+import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
 
 program
-  .description("Compares two configuration files and shows a difference.")
-  .version("0.0.1", "-v, --vers", "output the version number")
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --vers', 'output the version number')
   .option('-f, --format <type>', 'output format')
   .argument('<filepath1>')
-  .argument('<filepath2>')
+  .argument('<filepath2>');
 program.parse();
