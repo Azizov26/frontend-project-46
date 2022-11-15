22595bc976b22fb19d48e9c87e2a4ae6e34331c0
Aziz <azizov-aziz58@yandex.ru>
2022-11-15 20:48:13 +0500

    check

--- src/index.js
+++ src/index.js
@@ -2,56 +2,49 @@ import path from 'path';
 import fs from 'fs';
 import _ from 'lodash';
 
-const getAbsolutFilePath = (filepath) => path.resolve(process.cwd(), '_fixtures_', filepath);
-// находит путь, относительный и абсолютный
+const getAbsolutPath = (filepath) => path.resolve(process.cwd(), '_fixtures_', filepath);
 
-const readFile = (filepath) => fs.readFileSync(getAbsolutFilePath(filepath), 'utf8');
-// читает файл
+const JSONParse = (file) => JSON.parse(file);
 
-const parsesFile = (file) => JSON.parse(file);
-// распарсить файл с джейсона на обычный объект
+const readfile = (filepath) => fs.readFileSync(getAbsolutPath(filepath), 'utf8');
 
-const getDiffInformation = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  // забираем ключи
-  const keys = _.sortBy(_.union([...keys1, ...keys2]));
-  // сортируем по алфавитному порядку по уникальным значениям
-  const result = keys.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-    // у ключей берем значения
-    if (_.isEqual(value1, value2)) {
-      // проверка на эквивалетность . то что одинаковые данные
+const getDiffInformation = (file1, file2) => {
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
+
+  const FileKeys = _.sortBy(_.union([...keys1, ...keys2]));
+
+  const result = FileKeys.map((key) => {
+    const valueFile1 = file1[key];
+    const valueFile2 = file2[key];
+
+    if (_.isEqual(valueFile1, valueFile2)) {
       return {
-        type: 'nochanges', // тип - без измений
+        type: 'nochanges',
         key,
-        value: value1,
+        value: valueFile1,
       };
     }
-    if (value1 && value2 && value1 !== value2) {
-      // тип измненен
+    if (valueFile1 && valueFile2 && valueFile1 !== valueFile2) {
       return {
         type: 'changed',
         key,
-        value1,
-        value2,
+        valueFile1,
+        valueFile2,
       };
     }
-    if (!_.has(obj2, key)) {
-      // удалить , так как нет ключа во втором объекте
+    if (!_.has(file2, key)) {
       return {
         type: 'delited',
         key,
-        value: value1,
+        value: valueFile1,
       };
     }
-    if (!_.has(obj1, key)) {
-      // добавить так как ессть только один ключ похожих нет
+    if (!_.has(file1, key)) {
       return {
         type: 'added',
         key,
-        value: value2,
+        value: valueFile2,
       };
     }
     return null;
@@ -61,27 +54,25 @@ const getDiffInformation = (obj1, obj2) => {
 };
 
 const gendiff = (filepath1, filepath2) => {
-  const file1 = readFile(filepath1);
-  const file2 = readFile(filepath2);
+  const file1 = readfile(filepath1);
+  const file2 = readfile(filepath2);
 
-  const informationDiff = getDiffInformation(parsesFile(file1), parsesFile(file2));
-  // console.log(informationDiff);
-  const result = informationDiff.map((diff) => {
-    const diffType = diff.type;
-    switch (diffType) {
+  const infoGendiff = getDiffInformation(JSONParse(file1), JSONParse(file2));
+  const result = infoGendiff.map((item) => {
+    const itemType = item.type;
+    switch (itemType) {
       case 'delited':
-        return `  - ${diff.key}: ${diff.value}`;
+        return `  - ${item.key}: ${item.value}`;
       case 'nochanges':
-        return `    ${diff.key}: ${diff.value}`;
+        return `    ${item.key}: ${item.value}`;
       case 'changed':
-        return `  - ${diff.key}: ${diff.value1} \n  + ${diff.key}: ${diff.value2}`;
+        return `  - ${item.key}: ${item.valueFile1} \n  + ${item.key}: ${item.valueFile2} `;
       case 'added':
-        return `  + ${diff.key}: ${diff.value}`;
+        return `  + ${item.key}: ${item.value}`;
       default:
         return null;
     }
   });
-
   return `{\n${result.join('\n')}\n}`;
 };
 
